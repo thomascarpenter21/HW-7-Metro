efaa73c5fc118c4a5bec23d3a5e5dfd00fe58780
Thomas Carpenter <thomascarpenter@Thomass-MacBook-Pro.local>
2017-03-22 18:14:48 -0700

    push homwwork

--- assets/Moments.js
+++ assets/Moments.js
@@ -1,15 +1,4 @@
-/* global firebase moment */
-// Steps to complete:
 
-// 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
-
-// 1. Initialize Firebase
-// Initialize Firebase
 var config = {
     apiKey: "AIzaSyBK6dhH_1uZ4eC0xmi0wvoJBC_fuPk2BJw",
     authDomain: "metro-a47f4.firebaseapp.com",
